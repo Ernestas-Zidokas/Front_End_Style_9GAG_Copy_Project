b089a7e0b9c42cc1f81b95019afad43c9b28c1d2
Katamaranas <ernestas.zidokas@gmail.com>
2019-03-27 21:23:52 +0200

    infiniti scroll

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -6,15 +6,15 @@ $(function() {
 
         $('#favourite').attr('data-favourite', text).append(
             `<li class="side-menu__lists__list">
-                <a href="#">
-                    <img src="${img}">
-                    <span>${text}</span>
-                </a>
-                <a class="" href="#">
-                    <svg id="remove-favourite" class="svg-icon star-hover star">
-                        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#star"></use>
-                    </svg>
-                </a>
+            <a href="#">
+            <img src="${img}">
+            <span>${text}</span>
+            </a>
+            <a class="" href="#">
+            <svg id="remove-favourite" class="svg-icon star-hover star">
+            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#star"></use>
+            </svg>
+            </a>
             </li>`
         );
 
@@ -37,51 +37,97 @@ $(function() {
     });
 })
 
-// const content = document.querySelector('.middle-bar');
-// const api_url = "https://rickandmortyapi.com/api/character/";
-// fetch(api_url)
-//   .then((response) => response.json())
-//   .then((result) => {
-//       console.log(result);
-//     result.results.forEach((element) => {
-//       content.insertAdjacentHTML('beforeend', template(element.name, element.image))
-//     })
-//   })
-//   .catch((err) => console.log('ivyko erroras => ', err));
-
-  $('.middle-bar').infiniteScroll({
-    // options
-    path: function() {
-        var pageNumber = ( this.loadCount + 1 );
-        return `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
-    },
-    append: template(element.name, element.image),
-    history: false,
-  });
-
-  // element argument can be a selector string
-  //   for an individual element
-  var infScroll = new InfiniteScroll( '.container', {
-    // options
-  });
-
-
-
-  const sideContent = document.querySelector('.right-bar__lists');
-  const side_api_url = "https://rickandmortyapi.com/api/character/?page=2";
-  fetch(side_api_url)
-  .then((response) => response.json())
-  .then((result) =>{
-    result.results.forEach((item) => {
-      const side_template = `
-      <li class="right-bar__lists__list">
-        <a href="#">
-            <img src ="${item.image}">
-            <h3>Name: ${item.name}, Status: ${item.status}, Type: ${item.type}, Species: ${item.species}, Gender: ${item.Gender}</h3>
-        </a>
-      </li>
-      `;
-      sideContent.insertAdjacentHTML('beforeend', side_template)
+const content = document.querySelector('.middle-bar__content');
+const api_url = "https://rickandmortyapi.com/api/character/?page=1";
+fetch(api_url)
+.then((response) => response.json())
+.then((result) => {
+    result.results.forEach((element) => {
+        content.insertAdjacentHTML('beforeend', template(element.name, element.image))
     })
-  })
-  .catch((error) => console.log('ivyko erroras => ', error));
+})
+.catch((err) => console.log('ivyko erroras => ', err));
+
+
+const sideContent = document.querySelector('.right-bar__lists');
+const side_api_url = "https://rickandmortyapi.com/api/character/?page=2";
+fetch(side_api_url)
+.then((response) => response.json())
+.then((result) =>{
+    result.results.forEach((item) => {
+        const side_template = `
+        <li class="right-bar__lists__list">
+        <a href="#">
+        <img src ="${item.image}">
+        <h3>Name: ${item.name}, Status: ${item.status}, Type: ${item.type}, Species: ${item.species}, Gender: ${item.gender}</h3>
+        </a>
+        </li>
+        `;
+        sideContent.insertAdjacentHTML('beforeend', side_template)
+    })
+})
+.catch((error) => console.log('ivyko erroras => ', error));
+
+
+
+// $.fn.isOnScreen = function(){
+//     var win = $(window);
+//     var viewport = {
+//         top : win.scrollTop(),
+//         left : win.scrollLeft()
+//     };
+//     viewport.right = viewport.left + win.width();
+//     viewport.bottom = viewport.top + win.height();
+//
+//     var bounds = this.offset();
+//     bounds.right = bounds.left + this.outerWidth();
+//     bounds.bottom = bounds.top + this.outerHeight();
+//
+//     return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+// };
+//
+// let pageCounter = 1;
+// let praejo = false;
+// if (praejo === false) {
+//     $(window).scroll(function() {
+//         if ($('#test').isOnScreen() == true && praejo === false) {
+//             console.log(praejo);
+//             pageCounter++;
+//             console.log(pageCounter);
+//             console.log('test is on screen');
+//
+        //     const content = document.querySelector('.middle-bar__content');
+        //     const api_url = `https://rickandmortyapi.com/api/character/?page=${pageCounter}`;
+        //     fetch(api_url)
+        //     .then((response) => response.json())
+        //     .then((result) => {
+        //         result.results.forEach((element) => {
+        //             content.insertAdjacentHTML('beforeend', template(element.name, element.image))
+        //         })
+        //     })
+        //     .catch((err) => console.log('ivyko erroras => ', err));
+        // }
+//         praejo = true;
+//
+//         setTimeout(function() {
+//             praejo = false;
+//         }, 10000)
+//
+//     });
+// }
+
+$(window).scroll(function() {
+   if($(window).scrollTop() + $(window).height() == $(document).height()) {
+
+       const content = document.querySelector('.middle-bar__content');
+       const api_url = `https://rickandmortyapi.com/api/character/?page=2`;
+       fetch(api_url)
+       .then((response) => response.json())
+       .then((result) => {
+           result.results.forEach((element) => {
+               content.insertAdjacentHTML('beforeend', template(element.name, element.image))
+           })
+       })
+       .catch((err) => console.log('ivyko erroras => ', err));
+   }
+});
